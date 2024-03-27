bf2d7223dc678cc5e1941f68b12132e7e3a8b085
ayanDataLeads <153164601+ayanDataLeads@users.noreply.github.com>
2024-03-28 00:00:31 +0530

    Add files via upload

---
+++ Fact_Check.py
@@ -0,0 +1,28 @@
+from openai import OpenAI
+
+client = OpenAI(
+            api_key=os.environ.get("OPEN_AI_API_KEY")
+        )
+
+# Define the fact-checking function
+def check_fact(text, client):
+    prompt = text + "\nbased on the above message, give a fact-checking response in one paragraph.\
+             \nthe Fact check must be backed by scientific evidence, research, and studies. Look for information published by WHO and other reputed medical organisations.\
+             \nStart the response with 'The claim...'\
+             \nIn a new paragraph starting with 'Always remember', emphasize the importance of obtaining medical advice from certified health professionals rather than relying on social media or unverified sources. Stress that misleading information can be harmful to public health and it's crucial to ensure the accuracy of health-related information before sharing it with others."
+
+    prompt = str(prompt)
+    
+    try:
+        response = client.chat.completions.create(
+            model="gpt-4-1106-preview",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant named Dr. Eagle."},
+                {"role": "user", "content": prompt},
+            ]
+        )
+        response_data = response.choices[0].message.content
+        return response_data
+
+    except Exception as e:
+        return f"Error during fact-checking: {e}"
---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from fact_checker import check_fact
+from openai import OpenAI
+
+# Set up OpenAI GPT-4 API key (this should be kept secret, and you should consider using Streamlit's Secrets Management for production)
+client = OpenAI(
+    api_key='sk-your_secret_key_here'  # Replace with your secret key
+)
+
+st.title("Welcome to Dr. Eagle's Fact Checking Service")
+
+st.write("Hello! I'm Dr. Eagle, your fact-checking assistant. What do you want me to fact check?")
+
+# Create a text area for user input
+user_input = st.text_area("Enter your query below:", height=200)
+
+# Create a button for submitting the fact check request
+if st.button("Submit"):
+    with st.spinner('Dr. Eagle is busy fact-checking your claim. Please wait...'):
+        fact_check_result = check_fact(user_input, client)
+        st.write(fact_check_result)
